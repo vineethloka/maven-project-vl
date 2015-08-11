fddf574cd4f8fc16271840ecf0742855528643d1
unknown <90007602@WASJ46564.amtrak.ad.nrpc>
2015-08-11 09:46:39 -0400

    verify application/json header is present in response

--- src/test/java/HelloMavenTest.java
+++ src/test/java/HelloMavenTest.java
@@ -1,4 +1,7 @@
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 
@@ -9,6 +12,14 @@ public class HelloMavenTest extends FunctionalTestCase {
 		runFlowAndExpect("sayHello", "Hello Maven");
 	}
 	
+	@Test
+	public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+		MuleEvent event = runFlow("retrieveFlights");
+		String contentType = 
+				event.getMessage().getOutboundProperty("Content-Type");
+		assertEquals("applciation/json", contentType);
+		
+	}
 	
 	@Override
 	protected String getConfigFile() {
